1a5f4849d56b8bd302be55fbbb4c0177e30d131a
Sergey Mironenko <serg1prime@gmail.com>
2024-05-03 17:16:59 +0300

    Auth-Client: new commit

--- src/controllers/sendAuth.controller.js
+++ src/controllers/sendAuth.controller.js
@@ -6,12 +6,16 @@ export const sendAuthentication = async (res, user) => {
   const normalizedUser = userService.normalize(user);
   const { accessToken, refreshToken } = jwtService.sign(normalizedUser);
   
+  console.log(2)
+
   await tokenService.save(user.id, refreshToken);
   
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     maxAge: 60 * 60 * 24 * 1000,
   });
+
+  console.log(1)
   
   res.send({
     user: normalizedUser,
